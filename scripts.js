4073238a091a78987fa5635344655ef8cf3d160a
Nandomoreno-sudo <nandomorenomarin@gmail.com>
2025-03-09 01:19:48 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// JavaScript para validaciones o interacciones futuras
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    // Aquí se podría agregar validación del formulario, si es necesario
+    console.log('Formulario enviado');
+  });
+  
